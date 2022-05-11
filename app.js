893b927e2327198c7429adf3d246d926adf07e52
elene <elenejavaxishvili@yahoo.com>
2022-05-11 16:32:22 -0700

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds  = 10;
 
 const app = express();
 
@@ -40,31 +42,41 @@ app.get("/register", function (req, res) {
     res.render("register")
 });
 //to make password in hash and it is inreversable
+//hash is the password that is generated after salting 10 times
 app.post("/register", function (req, res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        });
     });
-
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
-    })
+    
 });
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
+    //rehashing const password = md5(req.body.password);
     User.findOne({email: username}, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
                 if(foundUser.password === password) {
-                    res.render("secrets")
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if (result === true) {
+                            res.render("secrets");
+                        }
+                    });
+                   
                 }
             }
         }
@@ -74,3 +86,7 @@ app.post("/login", function (req, res) {
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
+
+//md5(req.body.password)
+//found userFound = hash passworded is compared to hash genereated password
+// in bcrypt compare if the result from comparing is true, so if result is true and they are the same passwords then we can render the secrets route
