98478b6a2c736e1902a99ec64e4c8a93b3017b16
elene <elenejavaxishvili@yahoo.com>
2022-05-11 22:04:45 -0700

    cookies and session+passport

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 //const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds  = 10;
+// const bcrypt = require("bcrypt");
+// const saltRounds  = 10;
+const session = require ("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose"); 
 
 const app = express();
 
@@ -16,6 +19,19 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//set up a session
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//initialize passport
+app.use(passport.initialize());
+//use passport
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
@@ -23,12 +39,20 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
+//set up passport local mongoose
+userSchema.plugin(passportLocalMongoose);
+
 //const secret = "Thisisourlittlesecret";
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 //secret: secret - is the secret to use to encrypt the password
 
 const User = mongoose.model("User", userSchema);
+//identification of the password 386
+passport.use(User.createStrategy());
+//for sessions serialise gets message stores
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home")
@@ -39,48 +63,52 @@ app.get("/login", function (req, res) {
 });
 
 app.get("/register", function (req, res) {
-    res.render("register")
+    res.render("register");
 });
-//to make password in hash and it is inreversable
-//hash is the password that is generated after salting 10 times
+
+app.get("/secrets", function (req, res) {
+    //see if user is already logged in tahn go to secrets if not logg them in
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User ({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets")
-            }
-        });
-    });
+   User.register({username: req.body.username}, req.body.password, function (err, user) {
+       if (err) {
+           console.log(err);
+           res.redirect("/register")
+       } else {
+           passport.authenticate("local")(req, res, function () {
+               res.redirect("/secrets")
+           })
+       }
+   })
     
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    //rehashing const password = md5(req.body.password);
-    User.findOne({email: username}, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                if(foundUser.password === password) {
-                    bcrypt.compare(password, foundUser.password, function(err, result) {
-                        if (result === true) {
-                            res.render("secrets");
-                        }
-                    });
-                   
-                }
-            }
-        }
-    });
+   const user = new User ({
+       username: req.body.username,
+       password: req.body.password
+   })
+   //login and authenticate the user
+   req.login(user, function (err) {
+      if (err) {
+          console.log(err);
+      } else {
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets")
+        })
+      }
+   })
 });
 
 app.listen(3000, function () {
@@ -90,3 +118,44 @@ app.listen(3000, function () {
 //md5(req.body.password)
 //found userFound = hash passworded is compared to hash genereated password
 // in bcrypt compare if the result from comparing is true, so if result is true and they are the same passwords then we can render the secrets route
+// login route
+// const username = req.body.username;
+// const password = req.body.password;
+// //rehashing const password = md5(req.body.password);
+// User.findOne({email: username}, function (err, foundUser) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         if (foundUser) {
+//             if(foundUser.password === password) {
+//                 bcrypt.compare(password, foundUser.password, function(err, result) {
+//                     if (result === true) {
+//                         res.render("secrets");
+//                     }
+//                 });
+               
+//             }
+//         }
+//     }
+// });
+
+//register
+// res.render("register")
+// });
+// //to make password in hash and it is inreversable
+// //hash is the password that is generated after salting 10 times
+// app.post("/register", function (req, res) {
+//     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//         const newUser = new User ({
+//             email: req.body.username,
+//             password: hash
+//         });
+    
+//         newUser.save(function (err) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 res.render("secrets")
+//             }
+//         });
+//     });
